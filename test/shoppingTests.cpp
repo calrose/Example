b7846d4cc26879463e33de1cfa6947fc070cec61
calrose <calrose00@gmail.com>
2021-04-15 14:52:55 -0400

    Made some placeholders for generateList and addExpirationDate

--- test/shoppingTests.cpp
+++ test/shoppingTests.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
 
-TEST(shoppingTests, macroTest) {
-    EXPECT_TRUE(true);
+TEST(shoppingTests, shoppingListTest) {
+    //placeholder
+    SomeObj testList;
+    EXPECT_EQ("some item", testList.something());
 }
+
+TEST(shoppingTests, addExpirationDateTest) {
+    //placeholder
+    Someobj testList;
+    EXPECT_EQ("N/A", test.getExpirationDate());
+    testList.setExpirationDate("4/5/21");
+    EXPECT_EQ("4/5/21", testList.getExpirationDate());
+}
+
