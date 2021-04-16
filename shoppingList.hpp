02c2acddbe3505105c20b55a5e18dbcf252e0ddb
calrose <calrose00@gmail.com>
2021-04-16 11:07:39 -0400

    Made some functionality for generateOutput

--- shoppingList.hpp
+++ shoppingList.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 int generateOutput(std::istream &input, std::ostream &output)
 {
+    Document d;
+    d.parse(input);
 
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    output << buffer.GetString() << "\n";
 }
 
 int addItem(std::istream &input, std::istream &item)
